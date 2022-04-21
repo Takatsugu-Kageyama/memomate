1f3e20a8bdead6e3309efe2214a1ece0a24abc98
Takatugu-Kageyama <takatuguwork@gmail.com>
2022-04-21 20:12:01 +0900

    test

--- pages/Home/MainPage.tsx
+++ pages/Home/MainPage.tsx
@@ -1,22 +1,22 @@
 // import styles
-import styles from "../../styles/MainPage.module.scss";
+import styles from "../../styles/pages/MainPage.module.scss";
 // import components
 import RecentList from "../../components/InitalScreen/RecentList";
 import FavoriteList from "../../components/InitalScreen/FavoriteList";
 import RecentMemoList from "../../components/InitalScreen/RecentMemoList";
 
 const MainPage = () => {
-  return (
-    <>
-      <div className={styles.overall}>
-        {/*Recent List Area*/}
-        <RecentList />
-        {/*Favorite List Area*/}
-        <FavoriteList />
-        {/*Recent Memos' list*/}
-        <RecentMemoList />
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className={styles.overall}>
+                {/*Recent List Area*/}
+                <RecentList/>
+                {/*Favorite List Area*/}
+                <FavoriteList/>
+                {/*Recent Memos' list*/}
+                <RecentMemoList/>
+            </div>
+        </>
+    );
 };
 export default MainPage;
