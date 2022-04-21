9da738a30982cda7714e303f8f2158b98e6b3b6c
Takatugu-Kageyama <takatuguwork@gmail.com>
2022-04-21 18:45:18 +0900

    test

--- components/InitalScreen/RecentMemoList.tsx
+++ components/InitalScreen/RecentMemoList.tsx
@@ -1,16 +1,18 @@
+// import styles
 import styles from "../../styles/components/InitalScreen/RecentMemoList.module.scss";
-import RecentListCard from "./RecentListCard";
+// import component
+import RecentMemoListCard from "./RecentMemoListCard";
 
 const RecentMemoList = () => {
-  return (
-    <>
-      <div className={styles.recentMemoListWrap}>
-        <h2 className={styles.recentMemoListHeader}>最近作成したメモ</h2>
-        <div className={styles.recentContentsAreas}>
-          <RecentListCard />
-        </div>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className={styles.recentMemoListWrap}>
+                <h2 className={styles.recentMemoListHeader}>最近作成したメモ</h2>
+                <div className={styles.recentContentsAreas}>
+                    <RecentMemoListCard/>
+                </div>
+            </div>
+        </>
+    );
 };
 export default RecentMemoList;
--- components/InitalScreen/RecentMemoListCard.tsx
+++ components/InitalScreen/RecentMemoListCard.tsx
@@ -1,4 +1,28 @@
+// import styles
+import styles from "../../styles/components/InitalScreen/RecentMemoListCard.module.scss";
+
 const RecentMemoListCard = () => {
-  return <></>;
+    return <>
+        <div className={styles.memoListCardOverall}>
+            <div className={styles.memoListCardWrap}>
+                <p className={styles.listName}>リスト#1</p>
+            </div>
+            <div className={styles.memoListCardWrap}>
+                <p className={styles.listName}>リスト#1</p>
+            </div>
+            <div className={styles.memoListCardWrap}>
+                <p className={styles.listName}>リスト#1</p>
+            </div>
+            <div className={styles.memoListCardWrap}>
+                <p className={styles.listName}>リスト#1</p>
+            </div>
+            <div className={styles.memoListCardWrap}>
+                <p className={styles.listName}>リスト#1</p>
+            </div>
+            <div className={styles.memoListCardWrap}>
+                <p className={styles.listName}>リスト#1</p>
+            </div>
+        </div>
+    </>;
 };
 export default RecentMemoListCard;
