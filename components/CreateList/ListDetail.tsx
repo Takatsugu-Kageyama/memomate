f4eb29762dd577acef3c19e0d83220e81d3ceb4c
Takatugu-work <92166663+Takatugu-work@users.noreply.github.com>
2022-05-16 08:53:47 +0900

    test

--- components/CreateList/ListDetail.tsx
+++ components/CreateList/ListDetail.tsx
@@ -1,22 +1,9 @@
 //import styles
 import styles from "../../styles/components/CerateList/ListDetail.module.scss";
-// import types
-import {HTMLButtonEvent} from "../../util/TypeDefinition/EventSchema"
-import {Props} from "../../util/TypeDefinition/EmojiSchema";
-// import emoji-picker
-import {Picker} from "emoji-mart";
-import {useEffect, useState} from "react";
 
 const ListDetail = () => {
-    const [clickFlg, onClickFlg] = useState(false)
-    const DisplayEmojiCompo = () => {
-        onClickFlg(true);
-    }
-    useEffect(DisplayEmojiCompo)
-    return <div className={styles.overallListDetail} onClick={DisplayEmojiCompo}>
+    return <div className={styles.overallListDetail}>
         {/*Display List Icon*/}
-        <div className={styles.iconsArea}></div>
-        {clickFlg ? <Picker/> : null}
         {/*Display List title and favorite button*/}
         <div className={styles.titleAndFavoriteArea}></div>
     </div>
