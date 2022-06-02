e5b4ef26ccd60659b4c9fc6f3bcc047ec81d2bd0
Takatugu-work <92166663+Takatugu-work@users.noreply.github.com>
2022-06-03 08:02:13 +0900

    ・firebaseの作成

--- pages/CreateList.tsx
+++ pages/CreateList.tsx
@@ -1,69 +1,72 @@
 //import styles
 import styles from "../styles/pages/CreateList.module.scss";
-import React, { useCallback, useEffect, useState } from "react";
+import React, {useCallback, useEffect, useState} from "react";
 //import Chakra UI
-import { Input } from "@chakra-ui/react";
+import {Input} from "@chakra-ui/react";
 import Picker from "emoji-picker-react";
 import dynamic from "next/dynamic";
 import SelectedMemo from "../components/CreateList/SelectedMemo";
 
 const CreateList = () => {
-  const Picker = dynamic(() => import("emoji-picker-react"), { ssr: false });
-  //Create state
-  const [chosenEmoji, setChosenEmoji] = useState(null);
-  const [isOpenPiker, setIsOpenPiker] = useState(false);
+    const Picker = dynamic(() => import("emoji-picker-react"), {ssr: false});
 
-  const onEmojiClick = (
-    event: React.MouseEvent<Element, MouseEvent>,
-    emojiObject: any
-  ) => {
-    event.stopPropagation();
-    setChosenEmoji(emojiObject);
-    setIsOpenPiker(false);
-  };
-  const onClosePicker = useCallback(() => {
-    setIsOpenPiker(false);
-    document.removeEventListener("click", onClosePicker);
-  }, []);
-  const onOpenPicker = (e: React.MouseEvent<Element, MouseEvent>) => {
-    setIsOpenPiker(true);
-    document.addEventListener("click", onClosePicker);
-    e.stopPropagation();
-  };
+    //Create state
+    const [chosenEmoji, setChosenEmoji] = useState(null);
+    const [isOpenPiker, setIsOpenPiker] = useState(false);
 
-  useEffect(() => {
-    return () => {
-      document.removeEventListener("click", onClosePicker);
+    const onEmojiClick = (
+        event: React.MouseEvent<Element, MouseEvent>,
+        emojiObject: any
+    ) => {
+        event.stopPropagation();
+        setChosenEmoji(emojiObject);
+        setIsOpenPiker(false);
     };
-  }, [onClosePicker]);
+    const onClosePicker = useCallback(() => {
+        setIsOpenPiker(false);
+        document.removeEventListener("click", onClosePicker);
+    }, []);
+    const onOpenPicker = (e: React.MouseEvent<Element, MouseEvent>) => {
+        setIsOpenPiker(true);
+        document.addEventListener("click", onClosePicker);
+        e.stopPropagation();
+    };
 
-  return (
-    <div className={styles.overall}>
-      <div className={styles.upper}>
-        {/*emoji area*/}
-        <div className={styles.emojiArea} onClick={onOpenPicker}>
-          {chosenEmoji ? (
-            <span className={styles.emoji}>{chosenEmoji.emoji}</span>
-          ) : (
-            <img src="images/CreateList/non_icon.png" alt="" />
-          )}
-        </div>
-        <div className={styles.picker}>
-          {isOpenPiker ? (
-            <div className={styles.emojiList}>
-              <Picker onEmojiClick={onEmojiClick} />
+    useEffect(() => {
+        return () => {
+            document.removeEventListener("click", onClosePicker);
+        };
+    }, [onClosePicker]);
+
+    return (
+        <div className={styles.overall}>
+            <div className={styles.upper}>
+                {/*emoji area*/}
+                <div className={styles.emojiArea} onClick={onOpenPicker}>
+                    {chosenEmoji ? (
+                        <span className={styles.emoji}>{chosenEmoji.emoji}</span>
+                    ) : (
+                        <img src="images/CreateList/non_icon.png" alt=""/>
+                    )}
+                </div>
+                <div className={styles.picker}>
+                    {isOpenPiker ? (
+                        <div className={styles.emojiList} onClick={(e: React.MouseEvent<Element, MouseEvent>) => {
+                            e.stopPropagation()
+                        }}>
+                            <Picker onEmojiClick={onEmojiClick}/>
+                        </div>
+                    ) : null}
+                </div>
+                {/*Tittle Area*/}
+                <Input
+                    className={styles.titleInput}
+                    focusBorderColor="none"
+                    placeholder="タイトルを入力"
+                />
             </div>
-          ) : null}
+            <SelectedMemo/>
         </div>
-        {/*Tittle Area*/}
-        <Input
-          className={styles.titleInput}
-          focusBorderColor="none"
-          placeholder="タイトルを入力"
-        />
-      </div>
-      <SelectedMemo />
-    </div>
-  );
+    );
 };
 export default CreateList;
---
+++ util/Firebase/firenaseInit.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import {initializeApp} from "firebase/app";
+import {getAnalytics} from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCWWWiVfVs7yVTMx3PKzuZ8NnT7n_iUWNM",
+    authDomain: "memomate-f9d2e.firebaseapp.com",
+    projectId: "memomate-f9d2e",
+    storageBucket: "memomate-f9d2e.appspot.com",
+    messagingSenderId: "222465642902",
+    appId: "1:222465642902:web:4497da9f6e0f35eee0cdb7",
+    measurementId: "G-MZJCHKX5T3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
